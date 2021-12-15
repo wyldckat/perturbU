a3ba7b5101e00587c5423537cda5807fa4c84ced
Cyrille Bonamy <cyrille.bonamy@univ-grenoble-alpes.fr>
2021-12-16 00:56:11 +0100

    style

--- perturbUCylinder/perturbU.C
+++ perturbUCylinder/perturbU.C
@@ -63,74 +63,74 @@ int main(int argc, char *argv[])
         IOobject::MUST_READ
     );
 
-        Info<< "    Reading U" << endl;
-        volVectorField U(Uheader, mesh);
-        const scalar Retau = 300;
-        const scalar d = 7.54/(2000.0);
+    Info<< "Reading U" << endl;
+    volVectorField U(Uheader, mesh);
+    const scalar Retau = 300;
+    const scalar d = 7.54/(2000.0);
 
-        IOdictionary transportProperties
+    IOdictionary transportProperties
+    (
+        IOobject
         (
-            IOobject
-            (
-                "transportProperties",
-                runTime.constant(),
-                mesh,
-                IOobject::MUST_READ,
-                IOobject::NO_WRITE
-            )
-        );
-        dimensionedScalar nu
+            "transportProperties",
+            runTime.constant(),
+            mesh,
+            IOobject::MUST_READ,
+            IOobject::NO_WRITE
+        )
+    );
+    dimensionedScalar nu
+    (
+        transportProperties.lookup("nu")
+    );
+    dimensionedVector Ubar
+    (
+        transportProperties.lookup("Ubar")
+    );
+
+    const scalar utau = Retau*nu.value()/d;
+    //wall normal circulation
+    const scalar duplus = Ubar.value()[0]*0.5/utau;
+    //spanwise wavenumber: spacing z+ = 200
+    const scalar betaPlus = 2.0*constant::mathematical::pi*(1.0/200.0);
+    //const scalar sigma = 0.00055;
+    const scalar sigma = 0.0002;
+    //streamwise wave number: spacing x+ = 500
+    const scalar alphaPlus = 2.0*constant::mathematical::pi*(1.0/800.0);
+    const scalar epsilon = Ubar.value()[0]/20.0;
+
+    forAll(centers, celli)
+    {
+        scalar& Ux(U[celli].x());
+        vector cCenter = centers[celli];
+        scalar r = ::sqrt(::sqr(cCenter.y()) + ::sqr(cCenter.z()));
+        Ux = 2*mag(Ubar.value())*(1-::sqr(r/d));
+        scalar y = d - r;
+        r = r*Retau/d;
+        y = y*Retau/d;
+
+        scalar theta = ::atan(cCenter.y()/cCenter.z());
+        scalar x = cCenter.x()*Retau/d;
+
+
+        Ux = Ux + (utau*duplus/2.0)
+             *::cos(betaPlus*theta*r) *(y/30)
+             *::exp(-sigma*::sqr(y) + 0.5);
+        scalar utheta = epsilon*::sin(alphaPlus*x)*y
+                        *::exp(-sigma*::sqr(y));
+
+        vector tangential
         (
-            transportProperties.lookup("nu")
+            0, cCenter.y(), cCenter.z()
         );
-        dimensionedVector Ubar
-        (
-            transportProperties.lookup("Ubar")
-        );
+        tangential = tangential ^ vector(1,0,0);
+        tangential = tangential/mag(tangential);
 
-        const scalar utau = Retau*nu.value()/d;
-        //wall normal circulation
-        const scalar duplus = Ubar.value()[0]*0.5/utau;
-        //spanwise wavenumber: spacing z+ = 200
-        const scalar betaPlus = 2.0*constant::mathematical::pi*(1.0/200.0);
-        //const scalar sigma = 0.00055;
-        const scalar sigma = 0.0002;
-        //streamwise wave number: spacing x+ = 500
-        const scalar alphaPlus = 2.0*constant::mathematical::pi*(1.0/800.0);
-        const scalar epsilon = Ubar.value()[0]/20.0;
+        U[celli] = U[celli] + utheta*tangential;
 
-        forAll(centers, celli)
-        {
-            scalar& Ux(U[celli].x());
-            vector cCenter = centers[celli];
-            scalar r = ::sqrt(::sqr(cCenter.y()) + ::sqr(cCenter.z()));
-            Ux = 2*mag(Ubar.value())*(1-::sqr(r/d));
-            scalar y = d - r;
-            r = r*Retau/d;
-            y = y*Retau/d;
+    }
 
-            scalar theta = ::atan(cCenter.y()/cCenter.z());
-            scalar x = cCenter.x()*Retau/d;
-
-
-            Ux = Ux + (utau*duplus/2.0)
-                 *::cos(betaPlus*theta*r) *(y/30)
-                 *::exp(-sigma*::sqr(y) + 0.5);
-            scalar utheta = epsilon*::sin(alphaPlus*x)*y
-                            *::exp(-sigma*::sqr(y));
-
-            vector tangential
-            (
-                0, cCenter.y(), cCenter.z()
-            );
-            tangential = tangential ^ vector(1,0,0);
-            tangential = tangential/mag(tangential);
-
-            U[celli] = U[celli] + utheta*tangential;
-
-        }
-
-        U.write();
+    U.write();
 
     Info<< endl;
 
